28acb5f548a9bfbc81471de56c283e2b0d7ba29b
Sandip <sandip1805@gmail.com>
2022-07-07 16:20:08 +0530

    Day 14 - Basic Stack Implementation using array

---
+++ day_14_stack_using_array.py
@@ -0,0 +1,32 @@
+class Stack:
+    def __init__(self) -> None:
+        self.maxSize = 100
+        self.data = [None] * self.maxSize
+        self.top = -1
+    
+    def push(self, data):
+        if (self.top == self.maxSize-1):
+            raise Exception("Stack overflow!")
+        self.top += 1
+        self.data[self.top] = data
+    
+    def pop(self):
+        if (self.top == -1):
+            raise Exception("Stack is empty")
+        item = self.data[self.top]
+        self.top -= 1
+        return item
+    
+    def top(self):
+        if (self.top == -1):
+            raise Exception("Stack is empty")
+        return self.data[self.top]
+    
+    def printStack(self):
+        print(self.data)
+
+if __name__ == '__main__':
+    stack = Stack()
+    stack.push(1)
+    stack.push(2)
+    stack.printStack()
