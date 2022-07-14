c8f7aee434f0f8162ae5a83215b4114dfbf94ecc
Sandip <sandip1805@gmail.com>
2022-07-14 10:19:43 +0530

    Day 19 - Search, findmin and findheight implemented

--- day_18_binary_search_tree.py
+++ day_18_binary_search_tree.py
@@ -21,6 +21,38 @@ class BinarySearchTree:
     def insert(self, data):
         self.root = self.__insertHelper(self.root, data)
     
+    def __searchHelper(self, root, data):
+        if (root == None or root.data == data):
+            return root
+        if (data < root.data):
+            return self.__searchHelper(root.left, data)
+        else:
+            return self.__searchHelper(root.right, data)
+
+    def search(self, data):
+        return self.__searchHelper(self.root, data)
+
+    def __findMinHelper(self, root):
+        if (root == None):
+            return None
+        elif(root.left == None):
+            return root.data
+        else:
+            return self.__findMinHelper(root.left)
+
+    def findMin(self):
+        return self.__findMinHelper(self.root)
+
+    def __findHeight(self, root):
+        if (root == None):
+            return -1
+        leftWeight = self.__findHeight(root.left)
+        rightWeight = self.__findHeight(root.right)
+        return max(leftWeight, rightWeight) + 1
+
+    def findHeight(self):
+        return self.__findHeight(self.root)
+    
     def __inorder(self, root):
         if root:
             self.__inorder(root.left)
@@ -34,14 +66,20 @@ class BinarySearchTree:
 
 if __name__ == "__main__":
     print("Binary Search Tree!!!")
-    bst = BinarySearchTree()
+    bst = BinarySearchTree()    
     bst.insert(10)
     bst.insert(7)
-    bst.insert(14)
+    bst.insert(14)    
     bst.insert(8)
-    bst.insert(6)
+    bst.insert(6)    
     bst.insert(13)
+    bst.insert(19)
+    bst.insert(3)
     bst.insert(15)
-
+    bst.insert(4)
+    bst.insert(21)
     bst.printInOrder()
+    print(f'Search data: {bst.search(13).data}')
+    print(f'Find minimum data: {bst.findMin()}')
+    print(f'Find height of BST: {bst.findHeight()}')
         
